525064d81bf3c9ab00afaabc6d3e2b1f836361b7
dracito <dracito@gmail.com>
2020-04-05 10:25:35 +0200

    Adding Firebase Firestore

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBUflqLWnH4htuOoTA11HdPy1C0p6wHiY",
+    authDomain: "risk-game-manager.firebaseapp.com",
+    databaseURL: "https://risk-game-manager.firebaseio.com",
+    projectId: "risk-game-manager",
+    storageBucket: "risk-game-manager.appspot.com",
+    messagingSenderId: "579474754719",
+    appId: "1:579474754719:web:93550d46674fbbda7566aa",
+    measurementId: "G-GPB6TMFE83"
+  }
+};
